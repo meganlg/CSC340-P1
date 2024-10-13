477974c0b2654ccaad2f857b0397aeb3d8bda287
jghenke <justinhenke007@gmail.comm>
2024-10-13 11:56:30 -0500

    message about your commit

--- InventoryGen.cs
+++ InventoryGen.cs
@@ -1,1 +1,33 @@
 namespace DocumentNS;
+
+public class InventoryGen : Generation
+{
+    public InventoryGen() : base(3,3)
+    {
+
+    }
+        public override void SetTitle()
+    {
+        throw new NotImplementedException();
+    }
+    public override void SetHeaders()
+    {
+        throw new NotImplementedException();
+    }
+    public override void SetFields()
+    {
+        throw new NotImplementedException();
+    }
+    public override void PrintTitle()
+    {
+        throw new NotImplementedException();
+    }
+    public override void PrintHeaders()
+    {
+        throw new NotImplementedException();
+    }
+    public override void PrintFields()
+    {
+        throw new NotImplementedException();
+    }
+}
