10cead40476826bd6326f86c3fba8b78ed5b7a61
LilliAP <lillipmhsap@gmail.com>
2024-10-15 22:05:33 -0500

    Added data type documentation

--- obj/Debug/net8.0/CSC340-P1.AssemblyInfo.cs
+++ obj/Debug/net8.0/CSC340-P1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSC340-P1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aec57407fc0efa60a8a15b082eec81aa07fe6516")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ad372b6d5d9aa1e647bd8a41be2259b0e58511c")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSC340-P1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSC340-P1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
