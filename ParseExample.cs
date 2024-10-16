22276908d872455113ef223b8ddc134cb97b3cdf
meganlg <meganleagillen@gmail.com>
2024-10-15 19:04:34 -0500

    updated parse example

--- ParseExample.cs
+++ ParseExample.cs
@@ -4,32 +4,21 @@ using CsvHelper;
 
 class ParseExample{
 
-    static void parseTest(string path)
+    static void parseTest(string passinfilepath)
     {
-        List<Employee> employees = new List<Employee>();
-        using(var reader = new StreamReader(path))
-        {
-            try
-            {
-                using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
-                {
-                    var records = csv.GetRecords<Employee>();
-                    foreach (var record in records)
-                    {
-                        employees.Add(record);
-                    }
-                }
-            }
-            catch (CsvHelper.HeaderValidationException)
-            {
-                Console.WriteLine("File must contain the following headers: FirstName, LastName, ID, Pay, Position, PhoneNumber");
-            }
-            catch (CsvHelper.MissingFieldException)
-            {
-                Console.WriteLine("One or more items is missing a field.");
-                Console.WriteLine("Please ensure all fields are present.");
-            }
+        List<Inventory> inventoryList = new List<Inventory>();
+        InventoryParse inventoryParse = new InventoryParse(inventoryList){
+            FilePath = passinfilepath
+        };
+        inventoryParse.parseCsvFile();
+
+        // now the list will be populated
+        foreach (Inventory inv in inventoryList){
+            Console.WriteLine(inv.ID);
+            Console.WriteLine(inv.Category);
+            
         }
+
     }
 }
     
